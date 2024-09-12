8dec4c47fca8a42b5f16bc1224fa488a18bb7b41
Nikhil <guptanikhil801@gmail.com>
2024-09-12 23:21:17 +0530

    jwt configured

---
+++ Extensions/JwtAuthenticationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Product.Classes;
+using System.Text;
+namespace Product.Extensions;
+public static class JwtAuthenticationExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var jwtValues = configuration.GetSection("JwtValues").Get<JwtValues>();
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = jwtValues.Issuer,
+                    ValidAudience = jwtValues.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtValues.Key))
+                };
+            });
+
+        return services;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Product.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
